d5907caec7949ee1d30e00fc76d0744bd0b8b282
Bruno Lima <138326567+Brunooplima@users.noreply.github.com>
2025-09-18 16:41:55 -0300

    upload dos codigos de arvore que estavam no drive

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        ArvoreAvl tree = new ArvoreAvl();
+        
+        tree.inserir(3); 
+        tree.inserir(1);  
+        tree.inserir(6);  
+        tree.inserir(45);
+        tree.inserir(9);
+        tree.inserir(2);
+        tree.inserir(16);
+        tree.inserir(23);
+        
+        
+    }
+}
